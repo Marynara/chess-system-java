51c9d871fa9735c9846782048bc88a644c084cac
Marynara de Jesus <marynarajesus@hotmail.com>
2023-11-01 17:25:31 +0100

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,11 @@
 package application;
 
-import chess.ChessPiece;
-import chess.Color;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.Color;
 
 public class UI {
 
@@ -27,6 +30,18 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+		}
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -35,8 +50,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h ");
-
+		System.out.println("  a b c d e f g h");
 	}
 
 	private static void printPiece(ChessPiece piece) {
